8ee5d848278ffa0057efa66d5355b2ce026d0095
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-02-26 16:21:33 +0000

    Added input for the temp and the unity

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -6,7 +6,19 @@ namespace TempConvert
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+           // Create a program that asks for input of a temperature and origin unity
+           // "C" for Celsius ou "F" for Fahrenheit
+           // Using switch, the program converts a temperature for another unity and
+           // presents the result with 2 decimal houses;
+           // Formula: F = C x 1.8 + 32 e C = (F - 32)/1.8
+           // Additionally, the program presents the absolute value in converted temperature. If the unity inputted
+           // is not "C" or "F", the program prints "Invalid unity."
+
+            Console.WriteLine("Temperature:");
+            double temp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Unity (C/F):");
+            string unity = Console.ReadLine().ToUpper();
         }
     }
 }
