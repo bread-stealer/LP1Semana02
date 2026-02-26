0194c21a899f08daaf9300b48ef19afcbebabba6
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-02-26 16:34:35 +0000

    Added the conversion input logic

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -9,8 +9,10 @@ namespace TempConvert
            // Create a program that asks for input of a temperature and origin unity
            // "C" for Celsius ou "F" for Fahrenheit
            // Using switch, the program converts a temperature for another unity and
-           // presents the result with 2 decimal houses;
+           // presents the result with 2 decimal houses; The output should print the received value
+           // and the converted values: Examples( -40.00F = -40.00C)
            // Formula: F = C x 1.8 + 32 e C = (F - 32)/1.8
+           // 
            // Additionally, the program presents the absolute value in converted temperature. If the unity inputted
            // is not "C" or "F", the program prints "Invalid unity."
 
@@ -19,6 +21,21 @@ namespace TempConvert
 
             Console.WriteLine("Unity (C/F):");
             string unity = Console.ReadLine().ToUpper();
+
+            switch (unity)
+            {
+                case "C":
+                    double fahrenheit = temp * 1.8 + 32;
+                    Console.WriteLine($"{temp:F2} C = {fahrenheit:F2} F");
+                    Console.WriteLine($"Absolute value: {Math.Abs(fahrenheit):F2}F");
+                    break;
+                case "F":
+                    double celsius = (temp - 32) / 1.8;
+                    Console.WriteLine($"{temp:F2}F = {celsius:F2}C");
+                    Console.WriteLine($"Absolute value: {Math.Abs(celsius):F2}C");
+                    break;
+            }
+
         }
     }
 }
